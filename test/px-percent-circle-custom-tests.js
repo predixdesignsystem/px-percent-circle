e029bdc58d472ee9a5c07aefd62f6d74225a3f1f
Randy Askin <randall.askin@ge.com>
2016-08-09 10:16:14 -0700

    Additional unit tests

--- test/px-percent-circle-custom-tests.js
+++ test/px-percent-circle-custom-tests.js
@@ -5,7 +5,7 @@ function runCustomTests() {
   // This is the placeholder suite to place custom tests in
   // Use testCase(options) for a more convenient setup of the test cases
   suite('Custom Automation Tests for a default px-percent-circle', function() {
-    test('Check default value of px-percent-circle', function(done){
+    test('Check default value of first px-percent-circle', function(done){
       var percentEl = Polymer.dom(document).querySelector('#px-percent-circle-1'),
           percentValue = percentEl.value,
           percentLabel = percentEl.children[0].children[2].children[0].innerHTML;
@@ -13,14 +13,36 @@ function runCustomTests() {
       assert.equal(percentLabel, '0');
       done();
     });
-  });
-  suite('Custom Automation Tests for instantiated px-percent-circle', function() {
-    test('Check value of instantiated px-percent-circle', function(done){
+    test('Check calculated value of second px-percent-circle', function(done){
       var percentEl = Polymer.dom(document).querySelector('#px-percent-circle-2'),
           percentValue = percentEl.value,
           percentLabel = percentEl.children[0].children[2].children[0].innerHTML;
       assert.equal(percentValue, '10');
       assert.equal(percentLabel, '20');  // 10 / 50 = 20%
+      done();
+    });
+    test('Check negative value of third px-percent-circle', function(done){
+      var percentEl = Polymer.dom(document).querySelector('#px-percent-circle-3'),
+          percentValue = percentEl.value,
+          percentLabel = percentEl.children[0].children[2].children[0].innerHTML;
+      assert.equal(percentValue, '-10');
+      assert.equal(percentLabel, '0');
+      done();
+    });
+    test('Check default value of fourth px-percent-circle', function(done){
+      var percentEl = Polymer.dom(document).querySelector('#px-percent-circle-4'),
+          percentValue = percentEl.value,
+          percentLabel = percentEl.children[0].children[2].children[0].innerHTML;
+      assert.equal(percentValue, '100');
+      assert.equal(percentLabel, '100');
+      done();
+    });
+    test('Check default value of fourth px-percent-circle', function(done){
+      var percentEl = Polymer.dom(document).querySelector('#px-percent-circle-5'),
+          percentValue = percentEl.value,
+          percentLabel = percentEl.children[0].children[2].children[0].innerHTML;
+      assert.equal(percentValue, '20');
+      assert.equal(percentLabel, '100');
       done();
     });
   });
